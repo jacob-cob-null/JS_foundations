35e350bea6a73e24204a51177ec1f45fab224986
unknown <silveriolance06@gmail.com>
2025-08-02 16:57:23 +0800

    Finished tipCalculator

--- Functions/TipCalculator/script.js
+++ Functions/TipCalculator/script.js
@@ -1,15 +1,61 @@
+"use strict"
+
 const calculateBtn = document.getElementById('calculate');
 const res = document.getElementById('result');
 
-calculateBtn.addEventListener('click', () => calculateTip());
+//submit button
+calculateBtn.addEventListener('click', () => {
+  let amount = document.getElementById('amount').value;
+  let tip = document.getElementById('tip').value;
 
-function calculateTip() {
-  const amount = document.getElementById('amount').value;
-  const tip = document.getElementById('tip').value;
-
-  if (amount == '' && tip == '') {
-    alert('Fields should not be empty');
+  //check if empty
+  if (amount.trim() === '' || tip.trim() === '') {
+    alert('Inputs should not be empty');
     return;
   }
-  res.textContent = amount * tip;
+
+  //converted
+  amount = parseFloat(amount)
+  tip = parseFloat(tip)
+
+  //arrow function
+  let getTotal = (amount, calcTip) => amount + calcTip
+
+  let calcTip = calculateTip(amount, tip)//gets tip
+  let total = getTotal(amount, calcTip) //gets total
+
+
+  showResult(amount, calcTip.toFixed(2), total.toFixed(2))
+
+}
+);
+
+function showResult(amount, percent, result) {
+  const calcContainer = document.getElementById('calcContainer');
+  const totalContainer = document.getElementById('totalContainer');
+
+  if (!isNaN(result)) {
+    calcContainer.innerHTML = ''
+    calcContainer.innerHTML = `Amount: $${amount} <br> Tip: $${percent}`
+
+    totalContainer.innerHTML = ''
+    totalContainer.innerHTML = `Total: <br> $${result} `
+
+  }
+
+}
+function calculateTip(amount, tip) {
+
+  if (isNaN(amount) || isNaN(tip)) {
+    alert('Amount should only be digits');
+    return;
+  }
+  if (amount < 0 || tip < 0) {
+    alert('Inputs should not be negative');
+    return;
+  }
+
+  console.log(amount * (tip / 100))
+  return amount * (tip / 100);
+
 }
